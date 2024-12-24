4729f38319ecf46453fb5b174a7ef7dbc3bd65d4
sachin kumar <sachinkumarv007@gmail.com>
2024-12-24 15:24:16 +0530

    container config done

--- first.py
+++ first.py
@@ -77,3 +77,74 @@ print("OR opertor:", val1 or val2)
 
 
 # Type conversion 
+a4 = 2   #this integer value 
+b4 = 4.80  #this float value
+sum = a4 + b4 
+print(sum)  # total =6.80
+
+
+# a5 = "2"  # this string value / 
+# b5 = 4.45
+# sum1 = a5 + b5
+# print(sum1)  #output is given that error  it is not possible 
+#   can only concatenate str (not "float") to str
+
+
+#Type casting
+a6 = float("2")
+b6 = 4.45
+print(type(a6))  #<class 'float'>
+sum = a6+b6 
+print(sum)  #  total = 6.45
+
+#  it is not possiblte converted the string into the float
+# a6 = float("sachin")
+# b6 = 4.45
+# print(type(a6))  #<class 'float'>
+# sum = a6+b6 
+# print(sum)  #  total = 6.45
+
+
+
+# Input in python  taking from the user
+
+
+# name = input("enter the name:")
+# print("wecome",name)
+
+# #  result for input() is always a string
+
+# val = input("enter the some value")
+# print(type(val),val)  #"25", "99.99"
+
+# int(input())    /int
+# int("4")
+# val = int(input("enter some value"))
+# print(type(val),val)
+
+
+
+
+# print a program to input 2 number & print thier sum
+
+# first = int(input("enter the first number"))
+# second = int(input("enter the second number"))
+# print("sum=", first+second)
+
+
+# WAP to input side of a square & Print its Area.
+
+# side = int(input("Enter the Side "))
+# print("Area=",side*side) 
+
+
+#WAP to input 2 FLoating Point number & Print their Average.
+# first = float(input("Enter the firat number  "))
+# second = float(input("Enter the Second Number "))
+# print("Average=",(first+second)/2)
+
+
+# WAP  a program to 2 input 2 numbers,a and b. print True if a is greater than equal to b.if not Print False.
+first = int(input("enter the  First Number"))
+second = int(input("Enter the second Number"))
+print(first>=second)
