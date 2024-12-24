148d15347845d82924ccb8e08fc889a31149f135
sachin kumar <sachinkumarv007@gmail.com>
2024-12-24 14:16:10 +0530

    PYTHON SERIES

---
+++ first.py
@@ -0,0 +1,79 @@
+# Print a message (commented out)
+# print("hello sachin")
+
+# Variable declarations
+name = "sachin"
+age = 21
+old = False
+a2 = None
+price = 23.23
+
+# Check the datatype for each variable
+print(type(name))   # <class 'str'>
+print(type(age))    # <class 'int'>
+print(type(price))  # <class 'float'>
+print(type(old))    # <class 'bool'>
+print(type(a2))     # <class 'NoneType'>
+
+# Datatypes
+# Strings can be represented using "", '', or ''' '''
+name1 = "sachin"
+name2 = 'sachin'
+name3 = '''sachin'''
+print(name1)  # sachin
+print(name2)  # sachin
+print(name3)  # sachin
+
+# Print the sum of two numbers in Python
+a = 2
+b = 3
+sum = a + b
+print(sum)  # 5
+
+# Comments in Python
+# Single-line comments start with #
+# Multiline comments are enclosed in """ """
+
+# Operators
+# Arithmetic Operators
+a1 = 3
+b1 = 2
+print(a1 + b1)  # Addition: 5
+print(a1 - b1)  # Subtraction: 1
+print(a1 * b1)  # Multiplication: 6
+print(a1 / b1)  # Division: 1.5
+print(a1 % b1)  # Modulus: 1
+print(a1**b1)   #power:9
+
+
+
+# Relatioal operator :compared betweem two operator(==,!=,<,>,>=,<=)
+a2 = 50
+b2 = 20
+print(a2==b2)  #False
+print(a2!=b2) #true
+print(a2<b2)  #false
+print(a2>b2)  #true
+
+
+# Assignment Operator
+num = 10
+num = num + 10    #10+10=20
+print(num)
+
+
+# Logical Operator(it work on boolen Value)
+print(not False)
+print(not True)
+val1 = 50
+val2 = 30
+print(not False)
+print(not (a > b))
+val1 = True
+val2 = True
+print("AND opertor:", val1 and val2)
+
+print("OR opertor:", val1 or val2)
+
+
+# Type conversion 
